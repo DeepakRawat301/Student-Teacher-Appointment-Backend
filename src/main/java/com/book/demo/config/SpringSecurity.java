aa27546101cd10572f1c990886ef936eec45be92
Deepak Singh Rawat <deepaksinghrawat131@gmail.com>
2025-08-21 16:16:35 +0530

    commit first

--- src/main/java/com/book/demo/config/SpringSecurity.java
+++ src/main/java/com/book/demo/config/SpringSecurity.java
@@ -170,7 +170,7 @@ public class SpringSecurity
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // Your React frontend URL
+                        .allowedOrigins("http://localhost:3000","https://student-teacher-appointment-frontend-6u27divfa.vercel.app/") // Your React frontend URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
