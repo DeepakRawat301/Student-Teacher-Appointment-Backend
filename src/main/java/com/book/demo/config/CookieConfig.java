793cfde96cf2458f068f1f74ee994b4a08985821
Deepak Singh Rawat <deepaksinghrawat131@gmail.com>
2025-08-28 12:04:54 +0530

    Update CookieConfig.java

--- src/main/java/com/book/demo/config/CookieConfig.java
+++ src/main/java/com/book/demo/config/CookieConfig.java
@@ -1,12 +1,12 @@
 package com.book.demo.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.session.web.http.DefaultCookieSerializer;
 
 @Configuration
 public class CookieConfig {
+
     @Bean
     public DefaultCookieSerializer cookieSerializer() {
         DefaultCookieSerializer serializer = new DefaultCookieSerializer();
